85f13a7861f04454216b7dfda38a69665aa7c977
pulkitnsit2 <130029511+pulkitnsit2@users.noreply.github.com>
2023-04-18 00:29:25 +0530

    Update app.ts

--- node-typescript/src/app.ts
+++ node-typescript/src/app.ts
@@ -7,9 +7,17 @@ rookout.start();
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => res.send("Hello World"));
+app.get('/', (req, res) => {
+  console.log("Hello1")
+  console.log("Hello2")
+  res.send("Hello World");
+  console.log("Hello3")
+});
 app.get("/hello/:name", (req, res) => {
-    res.send("Hello, " + req.params.name);
+  console.log("Hello4")
+  console.log("Hello5")
+  res.send("Hello, " + req.params.name);
+  console.log("Hello6")
 });
 
-app.listen(5000);
+app.listen(3000);
